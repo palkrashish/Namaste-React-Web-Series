d8e0ee2e791271426eaa6738807c4f762c093b1b
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-04-01 10:20:16 +0530

    update: Functional and React element

--- Episode 03 - Laying the Foundation/Part 1/App.js
+++ Episode 03 - Laying the Foundation/Part 1/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
-import  ReactDOM  from 'react-dom';
-// LET SEE EXAMPLE OF USING JSX
-const jsxHeading = <h1>Namaste React using JSX</h1>
+import  ReactDOM  from 'react-dom/client';
+
+// LET SEE EXAMPLE OF USING JSX React element
+
+// const jsxHeading = (<h1>Namaste React using JSX</h1>); 
+
+
+// USING FUNCTIONAL COMPONENT
+
+const JsxHeading = () => {
+   return <h1>Namaste React using functional component</h1> 
+}
+
+// IF YOU USE ONLY () THIS bracket does not need to return and variable must be first letter capital letter
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(< JsxHeading />);
