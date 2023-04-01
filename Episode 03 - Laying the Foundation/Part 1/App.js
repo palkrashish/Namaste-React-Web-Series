45a95796115026237e9311d184afa5697f783888
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-04-01 11:27:36 +0530

    using other compoent design and use it

--- Episode 03 - Laying the Foundation/Part 1/App.js
+++ Episode 03 - Laying the Foundation/Part 1/App.js
@@ -5,11 +5,24 @@ import  ReactDOM  from 'react-dom/client';
 
 // const jsxHeading = (<h1>Namaste React using JSX</h1>); 
 
+const Title = () => {
+   return (
+    <h1>Hello I am Title runing from Title function</h1>
+    )
+}
+
 
 // USING FUNCTIONAL COMPONENT
 
 const JsxHeading = () => {
-   return <h1>Namaste React using functional component</h1> 
+   return(
+    <div id="container">
+    <h1>Namaste React using functional component</h1>
+    <Title />
+    
+    </div>
+   )
+    
 }
 
 // IF YOU USE ONLY () THIS bracket does not need to return and variable must be first letter capital letter
