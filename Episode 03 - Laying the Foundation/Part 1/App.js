86722cd1366c0c8e8aec7acb99f30caaf04fbd28
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-04-01 10:03:16 +0530

    update: using JSX see the browser output

--- Episode 03 - Laying the Foundation/Part 1/App.js
+++ Episode 03 - Laying the Foundation/Part 1/App.js
@@ -1,16 +1,7 @@
 import React from 'react'
 import  ReactDOM  from 'react-dom';
-const parent = React.createElement("div", { id: "parent" }, [
-//   console.log(parent)
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag child"),
-    React.createElement("h2", {}, "I am h2 tag child"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag of child2"),
-    React.createElement("h2", {}, "I am h2 tag of child2"),
-  ]),
-]);
+// LET SEE EXAMPLE OF USING JSX
+const jsxHeading = <h1>Namaste React using JSX</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
