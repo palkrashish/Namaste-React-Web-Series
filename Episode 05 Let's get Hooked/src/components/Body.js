f3b7f4c1a4431c4f1707b3b301bd22dae1fd2373
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-04-05 10:31:53 +0530

    feat: with the help of useState and filtered the data as rating

--- Episode 05 Let's get Hooked/src/components/Body.js
+++ Episode 05 Let's get Hooked/src/components/Body.js
@@ -1,86 +1,88 @@
 import RestaurentCard from "./RestaurentCard";
 import resList from "../utilis/mockData";
+import { useState } from 'react';
 
 const Body = () => {
-  let listOfRestaurants = [
-    {
+
+  const [listOfRestaurants ,setlistOfRestaurants] = useState([ {
+    data: {
       data: {
-        data: {
-          id: "21101",
-          name: "MCD",
-          totalRatingsString: "10000+ ratings",
-          cloudinaryImageId: "bobgejyxxdyhmzlcuvfj",
-          cuisines: [
-            "Mughlai",
-            "North Indian",
-            "Chinese",
-            "Biryani",
-            "Kebabs",
-            "Snacks",
-            "American",
-          ],
-          costForTwo: 30000,
-          costForTwoString: "₹300 FOR TWO",
-          avgRating: "3.8",
-        },
+        id: "21101",
+        name: "MCD",
+        totalRatingsString: "10000+ ratings",
+        cloudinaryImageId: "bobgejyxxdyhmzlcuvfj",
+        cuisines: [
+          "Mughlai",
+          "North Indian",
+          "Chinese",
+          "Biryani",
+          "Kebabs",
+          "Snacks",
+          "American",
+        ],
+        costForTwo: 30000,
+        costForTwoString: "₹300 FOR TWO",
+        avgRating: "3.8",
       },
     },
-    {
+  },
+  {
+    data: {
       data: {
-        data: {
-          id: "21102",
-          name: "KFC",
-          totalRatingsString: "10000+ ratings",
-          cloudinaryImageId: "bobgejyxxdyhmzlcuvfj",
-          cuisines: [
-            "Mughlai",
-            "North Indian",
-            "Chinese",
-            "Biryani",
-            "Kebabs",
-            "Snacks",
-            "American",
-          ],
-          costForTwo: 30000,
-          costForTwoString: "₹300 FOR TWO",
-          avgRating: "4.1",
-        },
+        id: "21102",
+        name: "KFC",
+        totalRatingsString: "10000+ ratings",
+        cloudinaryImageId: "bobgejyxxdyhmzlcuvfj",
+        cuisines: [
+          "Mughlai",
+          "North Indian",
+          "Chinese",
+          "Biryani",
+          "Kebabs",
+          "Snacks",
+          "American",
+        ],
+        costForTwo: 30000,
+        costForTwoString: "₹300 FOR TWO",
+        avgRating: "4.1",
       },
     },
-    {
+  },
+  {
+    data: {
       data: {
-        data: {
-          id: "21103",
-          name: "Jagat",
-          totalRatingsString: "10000+ ratings",
-          cloudinaryImageId: "bobgejyxxdyhmzlcuvfj",
-          cuisines: [
-            "Mughlai",
-            "North Indian",
-            "Chinese",
-            "Biryani",
-            "Kebabs",
-            "Snacks",
-            "American",
-          ],
-          costForTwo: 30000,
-          costForTwoString: "₹300 FOR TWO",
-          avgRating: "4.2",
-        },
+        id: "21103",
+        name: "Jagat",
+        totalRatingsString: "10000+ ratings",
+        cloudinaryImageId: "bobgejyxxdyhmzlcuvfj",
+        cuisines: [
+          "Mughlai",
+          "North Indian",
+          "Chinese",
+          "Biryani",
+          "Kebabs",
+          "Snacks",
+          "American",
+        ],
+        costForTwo: 30000,
+        costForTwoString: "₹300 FOR TWO",
+        avgRating: "4.2",
       },
     },
-  ];
+  },])
+  
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            listOfRestaurants = listOfRestaurants.filter(
+            const filterList = listOfRestaurants.filter(
               (res) => res.data.data.avgRating > 4
             );
             // in console we can change 
-            console.log(listOfRestaurants)
+            // console.log(listOfRestaurants)
+            setlistOfRestaurants(filterList)
           }}
         >
           Top Rated Restaurent
