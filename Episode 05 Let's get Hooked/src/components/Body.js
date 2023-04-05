c3ac5399b7393670501d34af49176e998fdb29ec
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-04-05 09:58:30 +0530

    feat:  in console disply only rating who is greater than 4

--- Episode 05 Let's get Hooked/src/components/Body.js
+++ Episode 05 Let's get Hooked/src/components/Body.js
@@ -1,16 +1,94 @@
 import RestaurentCard from "./RestaurentCard";
 import resList from "../utilis/mockData";
 
-export const Body = () => {
+const Body = () => {
+  let listOfRestaurants = [
+    {
+      data: {
+        data: {
+          id: "21101",
+          name: "MCD",
+          totalRatingsString: "10000+ ratings",
+          cloudinaryImageId: "bobgejyxxdyhmzlcuvfj",
+          cuisines: [
+            "Mughlai",
+            "North Indian",
+            "Chinese",
+            "Biryani",
+            "Kebabs",
+            "Snacks",
+            "American",
+          ],
+          costForTwo: 30000,
+          costForTwoString: "₹300 FOR TWO",
+          avgRating: "3.8",
+        },
+      },
+    },
+    {
+      data: {
+        data: {
+          id: "21102",
+          name: "KFC",
+          totalRatingsString: "10000+ ratings",
+          cloudinaryImageId: "bobgejyxxdyhmzlcuvfj",
+          cuisines: [
+            "Mughlai",
+            "North Indian",
+            "Chinese",
+            "Biryani",
+            "Kebabs",
+            "Snacks",
+            "American",
+          ],
+          costForTwo: 30000,
+          costForTwoString: "₹300 FOR TWO",
+          avgRating: "4.1",
+        },
+      },
+    },
+    {
+      data: {
+        data: {
+          id: "21103",
+          name: "Jagat",
+          totalRatingsString: "10000+ ratings",
+          cloudinaryImageId: "bobgejyxxdyhmzlcuvfj",
+          cuisines: [
+            "Mughlai",
+            "North Indian",
+            "Chinese",
+            "Biryani",
+            "Kebabs",
+            "Snacks",
+            "American",
+          ],
+          costForTwo: 30000,
+          costForTwoString: "₹300 FOR TWO",
+          avgRating: "4.2",
+        },
+      },
+    },
+  ];
   return (
     <div className="body">
-      <div className="search">
-        <input type="textarea" placeholder="Search Here" />
-        <button> Search</button>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            listOfRestaurants = listOfRestaurants.filter(
+              (res) => res.data.data.avgRating > 4
+            );
+            // in console we can change 
+            console.log(listOfRestaurants)
+          }}
+        >
+          Top Rated Restaurent
+        </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
-          <RestaurentCard key={restaurant.data.data.id} resData={restaurant} />
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurentCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
     </div>
